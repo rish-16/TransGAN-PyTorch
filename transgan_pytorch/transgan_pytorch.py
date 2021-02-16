97c5d57035531eae4f87c649924982d032869a3c
rish-16 <mail.rishabh.anand@gmail.com>
2021-02-17 00:40:35 +0800

    Add initial files to source

---
+++ transgan_pytorch/transgan_pytorch.py
@@ -0,0 +1,23 @@
+import torch
+from torch import nn
+import torch.nn.functional as F
+
+class Attention(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+        
+class EncoderLayer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass      
+        
+class Transformer(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass        
+
+class TransGAN(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
